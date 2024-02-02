f1ba84ec95a10582039e1568d4e212bc3e7ffbaf
Franco Alexander Ayala Palma <139255106+Fayalap@users.noreply.github.com>
2024-02-02 09:52:54 -0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,25 +1,3 @@
-// import { createStore, applyMiddleware } from 'redux';
-// import { persistStore, persistReducer } from 'redux-persist';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-// import rootReducer from './reducer';
-
-// // Configuración del middleware
-// const middleware = [];
-
-// // Configuración de persistencia
-// const persistConfig = {
-//   key: 'root',
-//   storage: AsyncStorage,
-// };
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// // Crea la tienda sin middleware y con el reducer persistido
-// const store = createStore(persistedReducer, applyMiddleware(...middleware));
-// const persistor = persistStore(store);
-
-// export { store, persistor };
-
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducer';
@@ -31,7 +9,7 @@ const enhancer = composeEnhancers(applyMiddleware(thunk));
 
 const persistConfig = {
   key: 'root',
-  storage: AsyncStorage, // Configura AsyncStorage como el motor de almacenamiento
+  storage: AsyncStorage,
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
